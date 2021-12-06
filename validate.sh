8fd84f1432da663652f0c77277d15beea4d2d526
Marcel Susky <marcel.susky@tu-dresden.de>
2021-12-06 09:06:37 +0100

    Adds JSON schema and FHIR resource validation using shell script

---
+++ validate.sh
@@ -0,0 +1,25 @@
+#/usr/bin/env sh
+
+# Check if ajv-cli for JSON schema validation is installed
+[ -z "$(command -v ajv)" ] && echo "Please install ajv-cli: https://www.npmjs.com/package/ajv-cli" && exit 1
+
+ajv -s fhir-schemas/fhir.schema.json \
+    -d "pathways/**/*.json" \
+    --remove-additional=failing \
+    --all-errors \
+    --messages=true
+
+[ $? -eq 0 ] || exit 1
+
+# Check if java runtime is installed
+[ -z "$(command -v java)" ] && echo "Please install java runtime environment: https://openjdk.java.net/install" && exit 1
+
+# Download FHIR validator if not exist and validate FHIR resources
+[ ! -f ./validator_cli.jar ] && wget -q https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar
+
+# Validate questionnaire against profiles and implementation guides
+java -jar validator_cli.jar pathways/**/*.json \
+     -version 4.0.1 \
+     -debug
+
+[ $? -eq 0 ] || exit 1
