a1f3ddd940e7e93589b14d96cef8310cfd8fc0a6
Marcel Susky <marcel.susky@tu-dresden.de>
2021-12-10 09:27:54 +0100

    Adds docker-compose file to test examples using HAPI FHIR server

--- publish.sh
+++ publish.sh
@@ -1,5 +1,15 @@
 #/usr/bin/env sh
 
+# Todo: Check arguments
+SERVER_ID=${1:-home}
+BASE_URL=${2:-http://localhost:8080/cqf-ruler}
+ACTION=${3:-create}
+PREFIX="[$(basename $0)] "
+
+echo "${PREFIX}Set server id:\t${SERVER_ID}"
+echo "${PREFIX}Set base url:\t${BASE_URL}"
+echo "${PREFIX}Set action:\t${ACTION}\n"
+
 # Check if curl, used for POST requests, is installed
 [ -z "$(command -v curl)" ] && echo "Please install curl" && exit 1
 
@@ -7,20 +17,20 @@
 [ -z "$(command -v jq)" ] && echo "Please install jq" && exit 1
 
 # POST each FHIR resource file to public HAPI FHIR R4 server
-for f in `ls -d ${PWD}/pathways/**/*.json`; do
-    let type=$(jq -r '.resourceType' ${f})
-    let id=$(jq -r '.id' ${f})
-    let res=$(
+for path in `ls -d ${PWD}/pathways/**/*.json`; do
+    type="$(jq -r '.resourceType' ${path})"
+    id="$(jq -r '.id' ${path})"
+    status=$(
         curl -sSL \
              -w "%{http_code}" \
              -o /dev/null \
-             -d "serverId=home_r4" \
+             -d "serverId=${SERVER_ID}" \
              -d "resource=${type}" \
              -d "resource-create-id=${id}" \
-             --data-urlencode "resource-create-body@${f}" \
-             -X POST https://hapi.fhir.org/update
+             --data-urlencode "resource-create-body@${path}" \
+             -X POST ${BASE_URL}/${ACTION}
     )
-    echo "${res}\t${f}"
+    echo "${PREFIX}${status}\t${id}\t${type}\t${path}"
 done
 
 [ $? -eq 0 ] || exit 1
